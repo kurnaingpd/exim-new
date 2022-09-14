8d404056aab1b0b3f9ec0e8e50ec38db3779cbcf
Kurnain Arsyi R <r.kurnain@gmail.com>
2022-09-14 16:08:15 +0700

    Tes git ignore

---
+++ application/controllers/Invoice.php
@@ -0,0 +1,51 @@
+<?php
+
+    Class Invoice extends CI_Controller
+    {
+        public function __construct()
+        {
+            parent::__construct();
+            if(!$this->session->userdata('logged_in')) redirect('/');
+            $this->load->model(['M_CRUD']);
+        }
+
+        public function index()
+        {
+            $datas['css'] = [
+                "text/css,stylesheet,".base_url("assets/adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css"),
+                "text/css,stylesheet,".base_url("assets/adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css"),
+                "text/css,stylesheet,".base_url("assets/adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css"),
+            ];
+
+            $datas['js'] = [
+                base_url("assets/adminlte/plugins/datatables/jquery.dataTables.min.js"),
+                base_url("assets/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"),
+                base_url("assets/adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js"),
+                base_url("assets/adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"),
+                base_url("assets/adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js"),
+                base_url("assets/adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"),
+                base_url("assets/adminlte/plugins/sweetalert/sweetalert.min.js"),
+                base_url("assets/js/invoice/list.js"),
+            ];
+            $datas['title'] = 'Export - Export Terms';
+            $datas['breadcrumb'] = ['Export', 'Transaction', 'Export Terms'];
+            $datas['header'] = 'Export terms list';
+            $datas['params'] = [
+                'list' => $this->M_CRUD->readData('view_trans_export_terms_list')
+            ];
+
+            $this->template->load('default', 'contents' , 'export/invoice/list', $datas);
+        }
+
+        public function add()
+        {
+
+        }
+
+        public function save()
+        {
+
+        }
+    }
+
+?>
