5c708c5b5387d6369266ae15e55a5d7ade31c2c0
Kurnain Arsyi R <r.kurnain@gmail.com>
2022-09-14 09:33:33 +0700

    Add config email to testing & production env

---
+++ application/config/testing/email.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config = array(
+    'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
+    'smtp_host' => 'smtp.office365.com', 
+    'smtp_port' => 587,
+    'smtp_user' => 'no-reply2@gonusa-distribusi.com',
+    'smtp_pass' => 'G4djah@2O22',
+    'smtp_crypto' => 'tls', //can be 'ssl' or 'tls' for example
+    'mailtype' => 'html', //plaintext 'text' mails or 'html'
+    'smtp_timeout' => '60', //in seconds
+    'charset' => 'utf-8',
+    'wordwrap' => TRUE,
+    'crlf' => '\r\n',
+    'newline' => '\r\n',
+    'priority' => 3
+);
