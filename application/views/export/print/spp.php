a847df3f12a12408852aa743db6b34ae96767942
Kurnain Arsyi R <r.kurnain@gmail.com>
2022-09-19 17:18:50 +0700

    Create template print spp

---
+++ application/views/export/print/spp.php
@@ -0,0 +1,65 @@
+<div id="container">
+    <h4 class="title">SURAT PERNYATAAN PRODUK</h4>
+
+    <div id="content" class="content">
+        <div id="detail" class="content">
+            <div style="margin-bottom: 1%;">Yang bertanda tangan dibawah ini :</div>
+            <div id="header" class="clearfix-content">
+                <div id="row">
+                    <div class="box-name">Nama</div>
+                    <div class="box-colon">:</div>
+                    <div class="box-value"></div>
+                </div>
+
+                <div id="row">
+                    <div class="box-name">Jabatan</div>
+                    <div class="box-colon">:</div>
+                    <div class="box-value"></div>
+                </div>
+            </div>
+            <div style="margin-top: 1%; margin-bottom: 1%;">Menyatakan bahwa produk sebagai berikut :</div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th colspan="3">PRODUK LOKAL</th>
+                        <th colspan="2">PRODUK ESKPOR</th>
+                    </tr>
+                    <tr>
+                        <th>Nama Dagang</th>
+                        <th>Nama Jenis</th>
+                        <th>No. MD</th>
+                        <th>Nama Dagang</th>
+                        <th>Nama Jenis</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="data-border">NO.</td>
+                        <td class="data-border">CARTOON BARCODE</td>
+                        <td class="data-border">CONTAINER</td>
+                        <td class="data-border">HS CODE</td>
+                        <td class="data-border">DESCRIPTION OF GOODS</td>
+                    </tr>
+                    <tr>
+                        <td class="data-border">NO.</td>
+                        <td class="data-border">CARTOON BARCODE</td>
+                        <td class="data-border">CONTAINER</td>
+                        <td class="data-border">HS CODE</td>
+                        <td class="data-border">DESCRIPTION OF GOODS</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div style="margin-top: 1%;">
+                <p>Deskripsi</p>
+            </div>
+        </div>
+
+        <div id="signature" style="margin-top: 2%; box-sizing: border-box; content: ''; clear: both; display: table;">
+            <div style="float: left; width: 20%; text-align: center;">
+                Kudus, <?=IndoDate?>
+                <div style="margin-top: 30%;"><u>Slamet Supriyadi</u></div>
+                <div style="font-weight: bold;">QA Manager</div>
+            </div>
+        </div>
+    </div>
+</div>
