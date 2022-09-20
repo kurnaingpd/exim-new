cbbe875b3476966ded9e5f3f2b1bcc070b4088ad
Kurnain Arsyi R <r.kurnain@gmail.com>
2022-09-20 13:52:22 +0700

    Merubah option value pada import menjadi dinammis dari DB

--- application/views/export/customer/add/import.php
+++ application/views/export/customer/add/import.php
@@ -4,8 +4,9 @@
             <label for="imp_bill" class="control-label">Bill of ladding</label>
             <select name="imp_bill" class="form-control select2bs4 import" id="imp_bill" disabled>
                 <option></option>
-                <option value="1">Send Original Doc</option>
-                <option value="2">Telex Release</option>
+                <?php foreach($params['import_bill'] as $rows) : ?>
+                    <option value="<?=$rows->id?>"><?=$rows->name?></option>
+                <?php endforeach; ?>
             </select>
         </div>
     </div>
@@ -15,9 +16,9 @@
             <label for="imp_packing" class="control-label">Packing list</label>
             <select name="imp_packing" class="form-control select2bs4 import" id="imp_packing" disabled>
                 <option></option>
-                <option value="1">Not Include</option>
-                <option value="2">Send Original Doc</option>
-                <option value="3">Send by Email</option>
+                <?php foreach($params['import_nonbill'] as $rows) : ?>
+                    <option value="<?=$rows->id?>"><?=$rows->name?></option>
+                <?php endforeach; ?>
             </select>
         </div>
     </div>
@@ -27,9 +28,9 @@
             <label for="imp_inv" class="control-label">Invoice</label>
             <select name="imp_inv" class="form-control select2bs4 import" id="imp_inv" disabled>
                 <option></option>
-                <option value="1">Not Include</option>
-                <option value="2">Send Original Doc</option>
-                <option value="3">Send by Email</option>
+                <?php foreach($params['import_nonbill'] as $rows) : ?>
+                    <option value="<?=$rows->id?>"><?=$rows->name?></option>
+                <?php endforeach; ?>
             </select>
         </div>
     </div>
@@ -39,9 +40,9 @@
             <label for="imp_inv_uv" class="control-label">Invoice under value</label>
             <select name="imp_inv_uv" class="form-control select2bs4 import" id="imp_inv_uv" disabled>
                 <option></option>
-                <option value="1">Not Include</option>
-                <option value="2">Send Original Doc</option>
-                <option value="3">Send by Email</option>
+                <?php foreach($params['import_nonbill'] as $rows) : ?>
+                    <option value="<?=$rows->id?>"><?=$rows->name?></option>
+                <?php endforeach; ?>
             </select>
         </div>
     </div>
@@ -51,9 +52,9 @@
             <label for="imp_coo" class="control-label">COO</label>
             <select name="imp_coo" class="form-control select2bs4 import" id="imp_coo" disabled>
                 <option></option>
-                <option value="1">Not Include</option>
-                <option value="2">Send Original Doc</option>
-                <option value="3">Send by Email</option>
+                <?php foreach($params['import_nonbill'] as $rows) : ?>
+                    <option value="<?=$rows->id?>"><?=$rows->name?></option>
+                <?php endforeach; ?>
             </select>
         </div>
     </div>
@@ -63,9 +64,9 @@
             <label for="imp_hc" class="control-label">Health certificate</label>
             <select name="imp_hc" class="form-control select2bs4 import" id="imp_hc" disabled>
                 <option></option>
-                <option value="1">Not Include</option>
-                <option value="2">Send Original Doc</option>
-                <option value="3">Send by Email</option>
+                <?php foreach($params['import_nonbill'] as $rows) : ?>
+                    <option value="<?=$rows->id?>"><?=$rows->name?></option>
+                <?php endforeach; ?>
             </select>
         </div>
     </div>
@@ -77,9 +78,9 @@
             <label for="imp_mat" class="control-label">Material savety data sheet</label>
             <select name="imp_mat" class="form-control select2bs4 import" id="imp_mat" disabled>
                 <option></option>
-                <option value="1">Not Include</option>
-                <option value="2">Send Original Doc</option>
-                <option value="3">Send by Email</option>
+                <?php foreach($params['import_nonbill'] as $rows) : ?>
+                    <option value="<?=$rows->id?>"><?=$rows->name?></option>
+                <?php endforeach; ?>
             </select>
         </div>
     </div>
@@ -89,9 +90,9 @@
             <label for="imp_coa" class="control-label">COA</label>
             <select name="imp_coa" class="form-control select2bs4 import" id="imp_coa" disabled>
                 <option></option>
-                <option value="1">Not Include</option>
-                <option value="2">Send Original Doc</option>
-                <option value="3">Send by Email</option>
+                <?php foreach($params['import_nonbill'] as $rows) : ?>
+                    <option value="<?=$rows->id?>"><?=$rows->name?></option>
+                <?php endforeach; ?>
             </select>
         </div>
     </div>
@@ -101,9 +102,9 @@
             <label for="imp_ps" class="control-label">Product spesification</label>
             <select name="imp_ps" class="form-control select2bs4 import" id="imp_ps" disabled>
                 <option></option>
-                <option value="1">Not Include</option>
-                <option value="2">Send Original Doc</option>
-                <option value="3">Send by Email</option>
+                <?php foreach($params['import_nonbill'] as $rows) : ?>
+                    <option value="<?=$rows->id?>"><?=$rows->name?></option>
+                <?php endforeach; ?>
             </select>
         </div>
     </div>
