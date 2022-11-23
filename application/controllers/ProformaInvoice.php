8469e6d9d295587f8f520cb6f9717c0848bb6b74
Kurnain Arsyi R <r.kurnain@gmail.com>
2022-11-23 11:08:57 +0700

    Proforma invoice/edit: PI di user Procurement masih error

--- application/controllers/ProformaInvoice.php
+++ application/controllers/ProformaInvoice.php
@@ -718,244 +718,47 @@
 
 
 
-        // public function edit($id)
-        // {
-        //     $datas['js'] = [
-        //         base_url("assets/adminlte/plugins/jquery-validation/jquery.validate.min.js"),
-        //         base_url("assets/adminlte/plugins/jquery-validation/additional-methods.min.js"),
-        //         base_url("assets/adminlte/plugins/sweetalert/sweetalert.min.js"),
-        //         base_url("assets/js/proforma/edit.js"),
-        //     ];
-        //     $datas['title'] = 'Export - Proforma Invoice';
-        //     $datas['breadcrumb'] = ['Export', 'Transaction', 'Proforma Invoice'];
-        //     $datas['header'] = 'Detail record';
-        //     $datas['params'] = [
-        //         'detail' => $this->M_CRUD->readDatabyID('view_trans_pi_detail', ['is_deleted' => '0', 'id' => $id]),
-        //         // 'category' => $this->M_CRUD->pi_category('view_print_trans_pi_category', ['pi_id' => $id]),
-        //         // 'item' => $this->M_CRUD->pi_item('view_print_trans_pi_detail', ['is_deleted' => '0', 'pi_id' => $id]),
-        //         'container' => $this->M_CRUD->readData('view_print_trans_pi_container', ['pi_id' => $id]),
-        //         'category' => $this->M_CRUD->readData('view_print_trans_pi_category', ['pi_id' => $id]),
-        //         'item' => $this->M_CRUD->readData('view_print_trans_pi_detail', ['is_deleted' => '0', 'pi_id' => $id]),
-        //         'summary' => $this->M_CRUD->readDatabyID('view_trans_pi_detail_summary', ['pi_id' => $id]),
-        //     ];
+        public function edit($id)
+        {
+            $datas['js'] = [
+                base_url("assets/adminlte/plugins/jquery-validation/jquery.validate.min.js"),
+                base_url("assets/adminlte/plugins/jquery-validation/additional-methods.min.js"),
+                base_url("assets/adminlte/plugins/sweetalert/sweetalert.min.js"),
+                base_url("assets/js/proforma/edit.js"),
+            ];
+            $datas['title'] = 'Export - Proforma Invoice';
+            $datas['breadcrumb'] = ['Export', 'Transaction', 'Proforma Invoice'];
+            $datas['header'] = 'Detail record';
+            $datas['params'] = [
+                'detail' => $this->M_CRUD->readDatabyID('view_trans_pi_detail', ['is_deleted' => '0', 'id' => $id]),
+                'container' => $this->M_CRUD->readData('view_print_trans_pi_container', ['pi_id' => $id]),
+                'category' => $this->M_CRUD->readData('view_print_trans_pi_category', ['pi_id' => $id]),
+                'item' => $this->M_CRUD->readData('view_print_trans_pi_detail', ['is_deleted' => '0', 'pi_id' => $id]),
+                'summary' => $this->M_CRUD->readDatabyID('view_trans_pi_detail_summary', ['pi_id' => $id]),
+            ];
 
-        //     $this->template->load('default', 'contents' , 'export/proforma/edit/index', $datas);
-        // }
+            $this->template->load('default', 'contents' , 'export/proforma/edit/index', $datas);
+        }
 
-        // public function edit_procurement()
-        // {
-        //     $post = $this->input->post();
-        //     $condition = ['id' => $post['id']];
-        //     $params = [
-        //         'freight_cost' => $post['freight_cost'],
-        //         'insurance' => $post['insurance'],
-        //         'updated_at' => date('Y-m-d H:i:s'),
-        //         'updated_by' => $this->session->userdata('logged_in')->id,
-        //     ];
+        public function edit_procurement()
+        {
+            $post = $this->input->post();
+            $condition = ['id' => $post['id']];
+            $params = [
+                'freight_cost' => $post['freight_cost'],
+                'insurance' => $post['insurance'],
+                'updated_at' => date('Y-m-d H:i:s'),
+                'updated_by' => $this->session->userdata('logged_in')->id,
+            ];
 
-        //     if($this->M_CRUD->updateData('trans_pi', $params, $condition)) {
-        //         $response = ['status' => 1, 'messages' => 'Proforma invoice has been updated successfully.', 'icon' => 'success', 'url' => 'export/proforma'];
-        //     } else {
-        //         $response = ['status' => 0, 'messages' => 'Proforma invoice has failed to update.', 'icon' => 'error'];
-        //     }
+            if($this->M_CRUD->updateData('trans_pi', $params, $condition)) {
+                $response = ['status' => 1, 'messages' => 'Proforma invoice has been updated successfully.', 'icon' => 'success', 'url' => 'export/proforma'];
+            } else {
+                $response = ['status' => 0, 'messages' => 'Proforma invoice has failed to update.', 'icon' => 'error'];
+            }
 
-        //     echo json_encode($response);
-        // }
-
-        // public function process($id)
-        // {
-        //     $datas['css'] = [
-        //         "text/css,stylesheet,".base_url("assets/adminlte/plugins/select2/css/select2.min.css"),
-        //         "text/css,stylesheet,".base_url("assets/adminlte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css"),
-        //     ];
-        //     $datas['js'] = [
-        //         base_url("assets/adminlte/plugins/select2/js/select2.full.min.js"),
-        //         base_url("assets/adminlte/plugins/jquery-validation/jquery.validate.min.js"),
-        //         base_url("assets/adminlte/plugins/jquery-validation/additional-methods.min.js"),
-        //         base_url("assets/adminlte/plugins/sweetalert/sweetalert.min.js"),
-        //         base_url("assets/js/proforma/process.js"),
-        //     ];
-        //     $datas['title'] = 'Export - Proforma Invoice';
-        //     $datas['breadcrumb'] = ['Export', 'Transaction', 'Proforma Invoice'];
-        //     $datas['header'] = 'Process';
-        //     $datas['params'] = [
-        //         'detail' => $this->M_CRUD->readDatabyID('view_trans_pi_detail', ['is_deleted' => '0', 'id' => $id]),
-        //         'container' => $this->M_CRUD->readData('view_print_trans_pi_container', ['pi_id' => $id]),
-        //         'category' => $this->M_CRUD->readData('view_print_trans_pi_category', ['pi_id' => $id]),
-        //         'item' => $this->M_CRUD->readData('view_print_trans_pi_detail', ['is_deleted' => '0', 'pi_id' => $id]),
-        //         'summary' => $this->M_CRUD->readDatabyID('view_trans_pi_detail_summary', ['pi_id' => $id]),
-        //         'categories' => $this->M_CRUD->readData('master_pi_item_category', ['is_deleted' => '0']),
-        //         'items' => $this->M_CRUD->readData('master_item', ['is_deleted' => '0']),
-        //         'item_revise' => $this->M_CRUD->readData('view_print_trans_pi_detail', ['is_deleted' => '0', 'pi_id' => $id]),
-        //         'cbm_revise' => $this->M_CRUD->readDatabyID('view_trans_pi_detail_item', ['is_deleted' => '0', 'pi_id' => $id]),
-        //     ];
-
-        //     if($datas['params']['detail']->pi_status_id == 7) {
-        //         $datas['status'] = $this->M_CRUD->readDataIn('master_pi_status', ['is_deleted' => '0', 'id' => ['3','6']]);
-        //     } else {
-        //         $datas['status'] = $this->M_CRUD->readDataIn('master_pi_status', ['is_deleted' => '0', 'id' => ['3','5','7']]);
-        //     }
-
-        //     $this->template->load('default', 'contents' , 'export/proforma/process/index', $datas);
-        // }
-
-        // public function update_process()
-        // {
-        //     $post = $this->input->post();
-        //     $condition = ['id' => $post['id']];
-        //     $param = [
-        //         'pi_status_id' => $post['status'],
-        //         'updated_at' => date('Y-m-d H:i:s'),
-        //         'updated_by' => $this->session->userdata('logged_in')->id,
-        //     ];
-
-        //     if($this->M_CRUD->updateData('trans_pi', $param, $condition)) {
-        //         $Grid = array();
-
-        //         foreach($_POST as $index => $value){
-        //             if(preg_match("/^grid_/i", $index)) {
-        //                 $index = preg_replace("/^grid_/i","",$index);
-        //                 $arr = explode('_',$index);
-        //                 $rnd = $arr[count($arr)-1];
-        //                 array_pop($arr);
-        //                 $idx = implode('_',$arr);
-                        
-        //                 $Grid[$rnd][$idx] = $value;
-        //                 if(!isset($Grid[$rnd]['pi_id'])){
-        //                     $Grid[$rnd]['pi_id'] = $post['id'];
-        //                 }
-        //             }
-        //         }
-
-        //         if(!empty($Grid)) {
-        //             foreach($Grid as $detail) {
-        //                 $params = [
-        //                     'pi_id' => $detail['pi_id'],
-        //                     'number_of_container' => $detail['container_no'],
-        //                     'pi_item_category_id' => $detail['item_category'],
-        //                     'item_id' => $detail['product'],
-        //                     'hs_code' => $detail['hs_code'],
-        //                     'qty' => $detail['qty'],
-        //                     'price' => $detail['price'],
-        //                     'cbm_item' => $detail['cbm'],
-        //                 ];
-        //                 $this->M_CRUD->insertData('trans_pi_detail', $params);
-        //             }
-        //         }
-
-        //         $paramHistory = [
-        //             'pi_id' => $post['id'],
-        //             'pi_status_id' => $post['status'],
-        //             'remark' => ($post['remark']?$post['remark']:NULL),
-        //             'created_by' => $this->session->userdata('logged_in')->id,
-        //         ];
-
-        //         $this->M_CRUD->insertData('trans_pi_history', $paramHistory);
-        //         $response = ['status' => 1, 'messages' => 'Proforma invoice has been updated successfully.', 'icon' => 'success', 'url' => 'export/proforma'];
-        //     } else {
-        //         $response = ['status' => 0, 'messages' => 'Proforma invoice has failed to update.', 'icon' => 'error'];
-        //     }
-
-        //     echo json_encode($response);
-        // }
-
-        // public function requests($id)
-        // {
-        //     $datas['css'] = [
-        //         "text/css,stylesheet,".base_url("assets/adminlte/plugins/select2/css/select2.min.css"),
-        //         "text/css,stylesheet,".base_url("assets/adminlte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css"),
-        //     ];
-        //     $datas['js'] = [
-        //         base_url("assets/adminlte/plugins/select2/js/select2.full.min.js"),
-        //         base_url("assets/adminlte/plugins/jquery-validation/jquery.validate.min.js"),
-        //         base_url("assets/adminlte/plugins/jquery-validation/additional-methods.min.js"),
-        //         base_url("assets/adminlte/plugins/sweetalert/sweetalert.min.js"),
-        //         base_url("assets/js/proforma/revise.js"),
-        //     ];
-        //     $datas['title'] = 'Export - Proforma Invoice';
-        //     $datas['breadcrumb'] = ['Export', 'Transaction', 'Proforma Invoice'];
-        //     $datas['header'] = 'Process';
-        //     $datas['params'] = [
-        //         'detail' => $this->M_CRUD->readDatabyID('trans_pi', ['is_deleted' => '0', 'id' => $id]),
-        //         'detail_value' => $this->M_CRUD->readDatabyID('view_trans_pi_detail', ['is_deleted' => '0', 'id' => $id]),
-        //         'customer' => $this->M_CRUD->readData('master_customer', ['is_deleted' => '0']),
-        //         'beneficiary' => $this->M_CRUD->readData('master_beneficiary', ['is_deleted' => '0']),
-        //         'load_port' => $this->M_CRUD->readData('master_loading_port', ['is_deleted' => '0']),
-        //         'container' => $this->M_CRUD->readData('master_container', ['is_deleted' => '0']),
-        //         'bank' => $this->M_CRUD->readData('master_bank', ['is_deleted' => '0']),
-        //         'currency' => $this->M_CRUD->readData('master_currency', ['is_deleted' => '0']),
-        //         'category' => $this->M_CRUD->readData('master_pi_item_category', ['is_deleted' => '0']),
-        //         'item' => $this->M_CRUD->readData('master_item', ['is_deleted' => '0']),
-        //         'item_value' => $this->M_CRUD->readData('view_print_trans_pi_detail', ['is_deleted' => '0', 'pi_id' => $id]),
-        //         'cbm_value' => $this->M_CRUD->readDatabyID('view_trans_pi_detail_item', ['is_deleted' => '0', 'pi_id' => $id]),
-        //     ];
-        //     $datas['chained'] = [
-        //         'discharge' => $this->M_CRUD->readData('view_customer_ship_detail', ['is_deleted' => '0', 'customer_id' => $datas['params']['detail']->customer_id]),
-        //         'freight' => $this->M_CRUD->readDatabyID('master_customer_freight', ['is_deleted' => '0', 'customer_id' => $datas['params']['detail']->customer_id]),
-        //         'coding_detail' => $this->M_CRUD->readData('view_master_customer_coding_detail', ['is_deleted' => '0', 'customer_id' => $datas['params']['detail']->customer_id]),
-        //         'coding' => $this->M_CRUD->readDatabyID('master_customer_coding', ['is_deleted' => '0', 'customer_id' => $datas['params']['detail']->customer_id]),
-        //     ];
-
-        //     if($datas['params']['detail']->pi_status_id == 7) {
-        //         $datas['status'] = $this->M_CRUD->readDataIn('master_pi_status', ['is_deleted' => '0', 'id' => ['3','6']]);
-        //     } else {
-        //         $datas['status'] = $this->M_CRUD->readDataIn('master_pi_status', ['is_deleted' => '0', 'id' => ['3','5','7']]);
-        //     }
-
-        //     $this->template->load('default', 'contents' , 'export/proforma/revise/index', $datas);
-        // }
-
-        // public function revise()
-        // {
-        //     $post = $this->input->post();
-        //     $condition = ['id' => $post['id']];
-        //     $param = [
-        //         'pi_status_id' => 1,
-        //         'updated_at' => date('Y-m-d H:i:s'),
-        //         'updated_by' => $this->session->userdata('logged_in')->id,
-        //     ];
-
-        //     if($this->M_CRUD->updateData('trans_pi', $param, $condition)) {
-        //         $Grid = array();
-
-        //         foreach($_POST as $index => $value){
-        //             if(preg_match("/^grid_/i", $index)) {
-        //                 $index = preg_replace("/^grid_/i","",$index);
-        //                 $arr = explode('_',$index);
-        //                 $rnd = $arr[count($arr)-1];
-        //                 array_pop($arr);
-        //                 $idx = implode('_',$arr);
-                        
-        //                 $Grid[$rnd][$idx] = $value;
-        //                 if(!isset($Grid[$rnd]['pi_id'])){
-        //                     $Grid[$rnd]['pi_id'] = $post['id'];
-        //                 }
-        //             }
-        //         }
-
-        //         if(!empty($Grid)) {
-        //             foreach($Grid as $detail) {
-        //                 $params = [
-        //                     'pi_id' => $detail['pi_id'],
-        //                     'number_of_container' => $detail['container_no'],
-        //                     'pi_item_category_id' => $detail['item_category'],
-        //                     'item_id' => $detail['product'],
-        //                     'hs_code' => $detail['hs_code'],
-        //                     'qty' => $detail['qty'],
-        //                     'price' => $detail['price'],
-        //                     'cbm_item' => $detail['cbm'],
-        //                 ];
-        //                 $this->M_CRUD->insertData('trans_pi_detail', $params);
-        //             }
-        //         }
-
-        //         $response = ['status' => 1, 'messages' => 'Proforma invoice has been updated successfully.', 'icon' => 'success', 'url' => 'export/proforma'];
-        //     } else {
-        //         $response = ['status' => 0, 'messages' => 'Proforma invoice has failed to update.', 'icon' => 'error'];
-        //     }
-
-        //     echo json_encode($response);
-        // }
+            echo json_encode($response);
+        }
     }
 
 ?>
